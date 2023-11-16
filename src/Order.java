a9768c8bae83edd0a323d2806ee2f4358bf4784e
He11Me <tasher.2018@inbox.ru>
2023-11-16 11:10:20 +0300

    modified order class

--- src/Order.java
+++ src/Order.java
@@ -1,17 +1,19 @@
 public class Order {
     private int count = 0;
+    private int copycount = count;
 
     public Order() {
 
     }
     public Order(int count) {
         this.count = count;
+
     }
 
 
     @Override
     public String toString() {
-        return "Order has count: " + count;
+        return "Order has count -  " + count;
     }
 
 }
